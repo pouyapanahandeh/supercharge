c13433e0248ecd8e9a12ea980bbc047a723aca54
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2019-01-04 15:46:02 +0100

    add the cards details include components plus install react-clean lib

--- front-end/react/supercharge/src/App.js
+++ front-end/react/supercharge/src/App.js
@@ -1,15 +1,30 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import Card from './components/card'
 import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
 
-class App {
-  render() {
-    return(
-      <div>
 
-      </div>
-    )
-  }
+
+function App() {
+  //glb function
+  const [flipped, setFlipped] = useState([])
+  const handleClick = (id) => setFlipped((flipped) => [...flipped, id])
+
+
+  return(
+    <div>
+      <h2> This is the memory Game</h2>
+        <Card
+          id={1}
+          width={100}
+          height={100}
+          back={'/img/back.png'}
+          front={'/img/reactu.png'}
+          flipped={flipped.includes(1)}
+          handleClick={() => handleClick(1)}
+        />
+    </div>
+  )
 }
 
 export default App;
