7b14de485e2186868af14333267793c3853accc3
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2019-01-04 15:34:43 +0100

    cards images added

--- front-end/react/supercharge/src/App.js
+++ front-end/react/supercharge/src/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
-import Route from 'react-router-dom/Route';
 
-
-class App extends Component {
-  render(){
+class App {
+  render() {
     return(
-      <Router>
-        <div className="App">
-          <Route path="/game" render={
-              ()=>{
-                return(<h3>welcome to the game</h3>);
-              }
-            }/>
-        </div>
-      </Router>
+      <div>
+
+      </div>
     )
   }
 }
